652b48f014e4eb89db139b58894248ad65e963cb
pushpendrarawat672 <124857937+pushpendrarawat672@users.noreply.github.com>
2024-10-18 08:56:34 +0530

    implement kadane's algo for max sum

--- max_sum.js
+++ max_sum.js
@@ -1,1 +1,21 @@
-console.log("hello")
+// Kadane's algo
+
+let arr =[-2,-3,4,-1,-2,1,5,3]
+
+
+let sum = 0;
+
+let mx = arr[0];
+
+
+for(let i =0; i < arr.length;i++){
+    sum+=arr[i];
+
+    mx = Math.max(mx, sum)
+
+    if(sum < 0){
+        sum=0
+    }
+}
+
+console.log(mx)

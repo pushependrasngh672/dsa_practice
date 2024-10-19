13b4351a4bd9a7b61da956835693c72933f71b54
pushpendrarawat672 <124857937+pushpendrarawat672@users.noreply.github.com>
2024-10-19 19:41:09 +0530

    remove comment

--- prefixsum/prefix_sum_array.js
+++ prefixsum/prefix_sum_array.js
@@ -1,10 +1,6 @@
 let ar = [2,4,1,6,8,3];
-
 let pSum = [...ar]
-// let sum = ar[0]
-// pSum.push(ar[0])
 for(let i =1; i < ar.length ; i++){
-    // console.log(ar[i - 1])
     pSum[i] += pSum[i - 1]
 
 }
